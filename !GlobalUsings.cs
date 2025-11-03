2328dd66dfa101534df4efa3f77bb841eeb0a3cd
JesOb <JesObertyukh@gmail.com>
2025-11-03 11:12:17 +0100

    Changed improved naming

--- !GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using UnityEngine;
-
-global using SGS = Flexy.GameSettings.Service_GameSettings;
---
+++ GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using UnityEngine;
+
+global using SGS = Flexy.GameSettings.Service_GameSettings;
